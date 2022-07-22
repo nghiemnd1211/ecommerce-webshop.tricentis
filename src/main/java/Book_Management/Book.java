85f13f07198298219201e2142c8f3e0cabb6788e
NghiemND <ducnghiem1211@gmail.com>
2022-07-22 18:17:56 +0700

    Project Book Management

---
+++ src/main/java/Book_Management/Book.java
@@ -0,0 +1,59 @@
+package Book_Management;
+
+public class Book {
+    private String title;
+    private String authorName;
+    private int ISBN;
+    private int year;
+
+    public Book(){
+    }
+    public Book(String title, String authorName, int ISBN, int year) {
+        this.title = title;
+        this.authorName = authorName;
+        this.ISBN = ISBN;
+        this.year = year;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", ISBN=" + ISBN +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/Book_Management/Readme.md
@@ -0,0 +1,6 @@
+This application has a menu in console that allows user to have below feature:
+- Add new book
+- Save book into book list and into a file
+- Retrieve book from Title
+- Retrive book from ISBN
+- Retrive book from Author Name
